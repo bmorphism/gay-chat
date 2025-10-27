146b065d5a54a5b5de487c7fd5d1a5f21661be33
David Thompson <dthompson2@worcester.edu>
2025-10-27 08:29:25 -0400

    Add licensing headers.

--- crdt.scm
+++ crdt.scm
@@ -1,9 +1,24 @@
+;;; Operation-based CRDT
+;;; Copyright (C) 2025 David Thompson <dave@spritely.institute>
+;;;
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;;
+;;;    http://www.apache.org/licenses/LICENSE-2.0
+;;;
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions and
+;;; limitations under the License.
+
 ;;; Commentary:
-;;
-;; A simple operation-based CRDT that uses vector clocks and an
-;; append-only event log.  Operations are processed in causal order.
-;; Replicas query each other to obtain new events.  Those replicas
-;; then initiate a pull of missing events.
+;;;
+;;; A simple operation-based CRDT with an append-only event log.
+;;; Operations are processed in causal order.
+;;;
+;;; Code:
 
 (define-module (crdt)
   #:use-module ((goblins) #:hide ($))
--- hlc.scm
+++ hlc.scm
@@ -1,8 +1,29 @@
+;;; Hybrid logical clock
+;;; Copyright (C) 2025 David Thompson <dave@spritely.institute>
+;;;
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;;
+;;;    http://www.apache.org/licenses/LICENSE-2.0
+;;;
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions and
+;;; limitations under the License.
+
 ;;; Commentary:
-;;
-;;
-;; Hybrid logical clocks are a tuple of real time, logical time, and a
-;; node ID.  Despite incorporating real time, this clock is monotonic.
+;;;
+;;; Hybrid logical clocks are a tuple of real time, logical time, and
+;;; a string ID.  Despite incorporating real time, this clock is
+;;; monotonic.  A partial ordering can be formed by comparing the
+;;; clock's real and logical components, in that order.  A total
+;;; ordering can be formed by incorporating the clock ID as a
+;;; tie-breaker.
+;;;
+;;; Code:
+
 (define-module (hlc)
   #:use-module (ice-9 match)
   #:export (make-clock
